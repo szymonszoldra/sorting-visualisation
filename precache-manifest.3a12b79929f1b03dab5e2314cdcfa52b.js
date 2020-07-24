c644385fe83bbfd12a6beefb8d24bf7b78bd3ee8
szymonszoldra <szymon.szoldra@gmail.com>
2020-07-24 11:05:30 +0200

    Updates

---
+++ precache-manifest.3a12b79929f1b03dab5e2314cdcfa52b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a0289f237c93f963b207a1b0e39773c",
+    "url": "/sorting-visualisation/index.html"
+  },
+  {
+    "revision": "c29694c4532aa9c3c0af",
+    "url": "/sorting-visualisation/static/css/main.e9ea4215.chunk.css"
+  },
+  {
+    "revision": "a16e2aa7b4a8ee2744f8",
+    "url": "/sorting-visualisation/static/js/2.54ef770e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sorting-visualisation/static/js/2.54ef770e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29694c4532aa9c3c0af",
+    "url": "/sorting-visualisation/static/js/main.79a7998c.chunk.js"
+  },
+  {
+    "revision": "4ed8cc29e75cd73302db",
+    "url": "/sorting-visualisation/static/js/runtime-main.8784d14e.js"
+  }
+]);
